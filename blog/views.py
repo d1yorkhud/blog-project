07295aa86c16f2c7347e73159acdbb9a014c47fb
Diyor <d1yorkhud@gmail.com>
2026-04-09 03:22:34 +0900

    shitty changes

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 class BlogListView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'post_detail.html'

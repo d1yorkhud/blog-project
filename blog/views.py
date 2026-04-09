65ffa59b7ee184885a014b48d9db2d073bec02b9
Diyor <d1yorkhud@gmail.com>
2026-04-10 03:34:15 +0900

    fixing issues

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,9 @@ from .models import Post
 # Create your views here.
 class BlogListView(ListView):
     model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
     template_name = 'post_detail.html'
+    context_object_name = 'post'
